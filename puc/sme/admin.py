6b743b21515e6251f884f0bee2a13ecfad615b39
Gustavo Soares <gustavosoares@gmail.com>
2009-10-14 10:13:15 -0300

    adicao da tabela coluna no admin

--- puc/sme/admin.py
+++ puc/sme/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 #from django.contrib.auth.models import User
-from puc.sme.models import Monitor, Monitorhelp, Alarme, Produto, Sme
+from puc.sme.models import Monitor, Coluna, Alarme, Produto, Sme
 
 class MonitorAdmin(admin.ModelAdmin):
 	list_display = ('mon_id', 'mon_nome', 'mon_tabela', 'mon_status', 'alm_id', 'mon_alarmar')
@@ -8,10 +8,18 @@ class MonitorAdmin(admin.ModelAdmin):
 	list_filter = ('alm_id', 'mon_nome',)
 	ordering = ('-alm_id',)
 
+class ColunaAdmin(admin.ModelAdmin):
+	list_display = ('mon_id', 'col_cabecalho', 'col_nomefisico', 'col_tipo', 'col_ordem',)
+	search_fields = ('mon_id',)
+	list_filter = ('mon_id', 'col_tipo',)
+	ordering = ('-mon_id',)
+		
+'''
 class MonitorhelpAdmin(admin.ModelAdmin):
 	list_display = ('mem_id', 'mon_id')
 	list_filter = ('mon_id',)
 	ordering = ('-mon_id',)
+'''
 
 class AlarmeAdmin(admin.ModelAdmin):
 	list_display = ('alm_id', 'alm_nome', 'prd_id', 'alm_status', 'alm_alarmar')
@@ -27,7 +35,8 @@ class ProdutoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Monitor, MonitorAdmin)
-admin.site.register(Monitorhelp, MonitorhelpAdmin)
+#admin.site.register(Monitorhelp, MonitorhelpAdmin)
 admin.site.register(Alarme, AlarmeAdmin)
 admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Coluna, ColunaAdmin)
 admin.site.register(Sme)
