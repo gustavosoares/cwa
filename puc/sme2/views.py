7615580b78e212dbb2fb0aff3cb32c06244c758f
Gustavo Soares <gustavosoares@gmail.com>
2009-10-16 08:41:32 -0300

    ...

--- puc/sme2/views.py
+++ puc/sme2/views.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 #importa modelos
 from puc.sme.models import Produto, Alarme, Monitor
 from puc.sme.core.repository import produto_repository
+from puc.sme2.core import util
 
 #retorna a listagem dos produtos no sme2
 def listar_produto(request):
@@ -20,7 +21,9 @@ def listar_produto(request):
 	produtos = produto_repository.get_produtos()
 	print produtos
 	
-	return render_to_response(template_name, { 'produtos' : produtos })
+	return render_to_response(template_name, 
+					{ 'produtos' : produtos,
+	 				'colors' : util.colors})
 
 def listar_alarme(request):
 	return HttpResponse("<h1>listar alarme</h1>")
