19f8ec61d91c75b8533afb23703b3c8bbad23a69
Gustavo Soares <gustavosoares@gmail.com>
2009-10-18 19:16:48 -0200

    ...

--- puc/relatorio/views.py
+++ puc/relatorio/views.py
@@ -11,13 +11,15 @@ from django.template.loader import render_to_string
 
 from puc import templates
 from puc.sme.models import Produto, Alarme, Monitor
-from puc.sme.core.repository import produto_repository
-from puc.sme.core.repository import monitor_repository
-from puc.sme.core.repository import alarme_repository
+from puc.sme.core.repository.produto_repository import ProdutoRepository
+from puc.sme.core.repository.monitor_repository import MonitorRepository
+from puc.sme.core.repository.alarme_repository import AlarmeRepository
 
 
 def index(request):
 	"""Pagina princial da aplicacao por gerar relatorio"""
 	##return HttpResponse("relatorio")
-	
-	return render_to_response(templates.TEMPLATE_RELATORIO_INDEX)
+	produtos = ProdutoRepository.get_produtos()
+
+	return render_to_response(templates.TEMPLATE_RELATORIO_INDEX, { 
+		'produtos': produtos})
