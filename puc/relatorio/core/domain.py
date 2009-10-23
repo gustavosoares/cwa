3737a6da4fa95460f9c794e9dfcf215f6c80e5d9
Gustavo Soares <gustavosoares@gmail.com>
2009-10-23 00:10:01 -0200

    blaaaaa

--- puc/relatorio/core/domain.py
+++ puc/relatorio/core/domain.py
@@ -54,17 +54,21 @@ class RelatorioGraficoLinha(Relatorio):
 	"""relatorio grafico de linha"""
 	def __init__(self):
 		"""docstring for __init__"""
+		from puc.relatorio.core import factory
 		Relatorio.__init__(self)
 		self.formato = 'grafico_linha'
 		self.template_name = 'relatorio/formato/grafico_linha.html'
+		self.grafico = factory.GraficoFactory.get_grafico('linha')
 
 class RelatorioGraficoBarra(Relatorio):
 	"""relatorio grafico de barra"""
 	def __init__(self):
 		"""docstring for __init__"""
+		from puc.relatorio.core import factory
 		Relatorio.__init__(self)
 		self.formato = 'grafico_barra'
 		self.template_name = 'relatorio/formato/grafico_barra.html'
+		self.grafico = factory.GraficoFactory.get_grafico('barra')
 
 				
 class Grafico(object):
@@ -81,17 +85,17 @@ class Grafico(object):
 		self.scale = "noscale" 
 		self.align = "middle"
 		self.response_type = "application/x-shockwave-flash"
-		self.pontos = [] #Ex.: pontos = [{'x' = '2','y' = '4'}]
+		self.pontos = {} #Ex.: pontos = {'variavel'= {'x' = [0,1,2],'y' = [1,2,3]}}
 		self.xml = None
 
-	def add_ponto(self, ponto):
-		#TODO: assert para ver se ponto é um dict
-		x = ponto.get('x', None)
-		y = ponto.get('y', None)
-		assert x != None, "o formato do ponto passado é ponto = {'x' = '2','y' = '4'}"
-		assert y != None, "o formato do ponto passado é ponto = {'x' = '2','y' = '4'}"
-		self.pontos.append(ponto)
+	def cria_variavel(self, variavel):
+		"""cria uma variavel no grafico"""
+		assert 0, 'o metodo para criacao de uma variavel no grafico precisa ser implementado'
 	
+	def add_ponto(self, variavel, *args):
+		"""adiciona um ponto no grafico"""
+		assert 0, 'o metodo para criacao de uma variavel no grafico precisa ser implementado'
+		
 	def xml(self):
 		"""retorna o xml para o grafico"""
 		assert 0, 'o metodo para obtencao do xml nao foi implementado'
@@ -116,6 +120,30 @@ class GraficoLinha(Grafico):
 		Grafico.__init__(self)
 		self.type = 'line'
 
+	def cria_variavel(self, variavel):
+		"""cria uma variavel no grafico"""
+		assert variavel != None, "por favor especificar o nome da variavel"
+		assert len(variavel) > 0, "por favor especificar o nome da variavel"
+		self.pontos[variavel]['x'] = []
+		self.pontos[variavel]['y'] = []
+		
+	def add_ponto(self, variavel, x, y):
+		"""adiciona um ponto no grafico"""
+		x_aux = self.pontos[variavel]['x']
+		y_aux = self.pontos[variavel]['y']
+		x_aux.append(x)
+		y_aux.append(y)
+		self.pontos[variavel]['x'] = x_aux
+		self.pontos[variavel]['y'] = y_aux
+		
+"""
+	def cria_variavel(self, variavel):
+		x = ponto.get('x', None)
+		y = ponto.get('y', None)
+		assert x != None, "o formato do ponto passado é ponto = {'x' = '2','y' = '4'}"
+		assert y != None, "o formato do ponto passado é ponto = {'x' = '2','y' = '4'}"
+		self.pontos.append(ponto)
+"""
 		
 class GraficoBarra(Grafico):
 	"""grafico de linha"""
