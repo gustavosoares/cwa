4edf43bfc0e593987340469d7c0b584a3bac074a
Gustavo Soares <gustavosoares@gmail.com>
2009-10-26 21:16:53 -0200

    campo read-only

--- puc/modelo/forms.py
+++ puc/modelo/forms.py
@@ -3,7 +3,9 @@ from django import forms
 from puc.modelo.models import Modelo
 
 class MyModeloAdminForm(forms.ModelForm):
-	metadado = forms.CharField(widget=forms.HiddenInput, label=u'Metadado', max_length=800)
+	#metadado = forms.CharField(widget=forms.HiddenInput, label=u'Metadado', max_length=800)
+	metadado = forms.CharField(widget=forms.TextInput(attrs={'size' : '100', 'readonly' : 'true'}), 
+	label=u'Metadado', max_length=800, help_text="Ordem das visoes. Campo somente leitura")
 	#metadado = forms.CharField(label=u'Metadado', max_length=800)
 	
 	class Meta:
