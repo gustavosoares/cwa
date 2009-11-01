cc32f20239dc8d775672fa308294996a2de13aee
Gustavo Soares <gustavosoares@gmail.com>
2009-11-01 11:15:05 -0200

    ...

--- puc/hiperbolica/core/repository.py
+++ puc/hiperbolica/core/repository.py
@@ -29,28 +29,28 @@ class ArvoreHiperbolicaRepository(Singleton):
 		contador = 1
 		#pega os produtos alarmando
 		produtos_alarmes = produto_repository.get_produtos_e_seus_alarmes()
-		print 'produtos e alarmes alarmando:\n %s\n' % produtos_alarmes
+		#print 'produtos e alarmes alarmando:\n %s\n' % produtos_alarmes
 		for produto_alarmes in produtos_alarmes:
 			#checo estado do produto
 			no_produto = domain.NoHiperbolico()
 			no_produto.id = contador
-			no_produto.name = produto_alarmes['produto'].prd_nome
+			no_produto.name = produto_alarmes['produto'].prd_nome.encode('utf-8')
 			no_produto.dim = 7
 			if produto_alarmes['produto'].prd_status == 'W':
-				no_produto.color = util.colors['warning']
+				no_produto.color = '#F90'
 			elif produto_alarmes['produto'].prd_status == 'A':
-				no_produto.color = util.colors['alarm']
+				no_produto.color = "#f00"
 			
 			for alarme in produto_alarmes['alarmes']:
 				contador = contador + 1
 				no_alarme = domain.NoHiperbolico()
 				no_alarme.id = contador
-				no_alarme.name = alarme.alm_nome
+				no_alarme.name = alarme.alm_nome.encode('utf-8')
 				no_alarme.dim = 6
 				if alarme.alm_status == 'W':
-					no_alarme.color = util.colors['warning']
+					no_alarme.color = '#F90'
 				elif alarme.alm_status == 'A':
-					no_alarme.color = util.colors['alarm']
+					no_alarme.color = "#f00"
 				
 				#pego os monitores do alarme
 				monitores = monitor_repository.get_monitor_alarmando_por_alarme_id(alarme.alm_id)
@@ -58,12 +58,12 @@ class ArvoreHiperbolicaRepository(Singleton):
 					contador = contador + 1
 					no_monitor = domain.NoHiperbolico()
 					no_monitor.id = contador
-					no_monitor.name = monitor.mon_nome
+					no_monitor.name = monitor.mon_nome.encode('utf-8')
 					no_monitor.dim = 5
 					if monitor.mon_status == 'W':
-						no_monitor.color = util.colors['warning']
+						no_monitor.color = '#F90'
 					elif monitor.mon_status == 'A':
-						no_monitor.color = util.colors['alarm']
+						no_monitor.color = "#f00"
 					
 					#adiciono filho(monitor no alarme)
 					no_alarme.add_children(copy.deepcopy(no_monitor))
