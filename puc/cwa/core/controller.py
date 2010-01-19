7143acfc1d50090ace13f748f4a8ad333d9afab2
Gustavo Soares <gustavosoares@gmail.com>
2010-01-19 17:27:40 -0200

    alterado o index cwa para ler do banco de dados

--- puc/cwa/core/controller.py
+++ puc/cwa/core/controller.py
@@ -12,10 +12,11 @@ from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 
 from puc import templates
-from puc.modelo.repository import ModeloRepository
+from puc.modelo.repository import ModeloRepository, WidgetRepository
 from puc.core import json
 
 modelo_repository = ModeloRepository()
+widget_repository = WidgetRepository()
 
 class CwaController(Controller):
 
@@ -24,11 +25,13 @@ class CwaController(Controller):
 
 		modelo = modelo_repository.get_modelo_ativo()
 		print '[CWA INDEX] modelo ativo: %s' % modelo
-
+		
+		widgets = widget_repository.get_todos()
+		
 		modelo_settings_json = modelo_repository.get_modelo_settings(modelo.nome)
 		print '[CWA INDEX] modelo settings: %s' % modelo_settings_json
 		#pego as informacoes do modelo configurado
-		return render_to_response(templates.TEMPLATE_CWA_INDEX, { 'settings' : modelo_settings_json})
+		return render_to_response(templates.TEMPLATE_CWA_INDEX, { 'settings' : modelo_settings_json, 'my_widgets' : widgets})
 
 	def widget(self):
 		return render_to_response('cwa/_teste/teste_widget.html')
