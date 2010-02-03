45632b134fc5072465aa16fc929d5fcd65f97182
Gustavo Soares <gustavosoares@gmail.com>
2010-02-02 22:42:33 -0200

    colocado variaveis para a pagina de preview

--- puc/templates.py
+++ puc/templates.py
@@ -10,16 +10,19 @@ TEMPLATE_LISTAR_MONITOR_DO_ALARME = 'sme2/listar_monitor_do_alarme.html'
 TEMPLATE_VER_EVENTO = 'sme2/ver_evento.html'
 TEMPLATE_SME2_ADMIN = 'admin/sme2/admin.html'
 SME2_ITEMS_POR_PAGINA = 15
+TEMPLATE_SME2_PREVIEW = 'sme2/preview.html'
 
 #VISAO RELATORIO
 TEMPLATE_RELATORIO_INDEX = 'relatorio/index.html'
 TEMPLATE_RELATORIO_INDEX_GRAFICO = 'relatorio/index-grafico.html'
 TEMPLATE_RELATORIO_INFO = 'relatorio/info.html'
+TEMPLATE_RELATORIO_PREVIEW = 'relatorio/preview.html'
 
 #VISAO HIERARQUICA
 TEMPLATE_HIPERBOLICA_INDEX = 'hiperbolica/index.html'
 TEMPLATE_HIPERBOLICA_MOSTRAR_ARVORE = 'hiperbolica/mostrar_arvore.html'
 TEMPLATE_HIPERBOLICA_INFO = 'hiperbolica/info.html'
+TEMPLATE_HIPERBOLICA_PREVIEW = 'hiperbolica/preview.html'
 
 #CWA
 TEMPLATE_CWA_INDEX = 'cwa/index.html'
